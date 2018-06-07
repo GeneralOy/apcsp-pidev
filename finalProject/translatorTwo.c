5fee754ffc3a852de77bb4c05143958c337cf2e7
League <League@AdminisorsiMac2.attlocal.net>
2018-06-07 16:51:27 -0700

    asdf
    new stuff; attempt @ decToBin

--- finalProject/translatorTwo.c
+++ finalProject/translatorTwo.c
@@ -1,90 +1,74 @@
 #include <stdio.h>
+#include <math.h>
 
-int decToBinary(int n/*, int notationBase*/)
+int reverseDec(int input)
 {
-	int returningInt;
-	for(int i = 31; i >= 0; i--) {
-	int k = n >> i;
-	if(k = 1)
-	{
-		printf("1");
-		returningInt += k * 10^i;
-	}/*else if(k = 2)
-	{
-		printf("2");
-		returningInt += k * 10^i;
-	}else if(k = 3)
-	{
-		printf("3");
-		returningInt += k * 10^i;
-	}else if(k = 4)
-	{
-		printf("4");
-		returningInt += k * 10^i;
-	}else if(k = 5)
-	{
-		printf("5");
-		returningInt += k * 10^i;
-	}else if(k = 6)
-	{
-		printf("6");
-		returningInt += k * 10^i;
-	}else if(k = 7)
-	{
-		printf("7");
-		returningInt += k * 10^i;
-	}else if(k = 8)
-	{
-		printf("8");
-		returningInt += k * 10^i;
-	}else if(k = 9)
-	{
-		printf("9");
-		returningInt += k * 10^i;
-	}/*if(k = 10)
-	{
-		printf("a");
-		returningInt += k * 10^i;
-	}if(k = 11)
-	{
-		printf("b");
-		returningInt += 1 * 10^i;
-	}if(k = 12)
-	{
-		printf("c");
-		returningInt += 1 * 10^i;
-	}if(k = 13)
-	{
-		printf("d");
-		returningInt += 1 * 10^i;
-	}if(k = 14)
-	{
-		printf("e");
-		returningInt += 1 * 10^i;
-	}if(k = 15)
-	{
-		printf("f");
-		returningInt += 1 * 10^i;
-	}if(k = 16)
-	{
-		printf("g");
-		returningInt += 1 * 10^i;
-	}*/else
-	{
-		printf("0");
+	int reversedNum = 0;
+	while(input != 0) {
+	reversedNum = 10*(reversedNum) + (input%10);
+	input = input/10;
 	}
-	}
-	printf("\n");
+	return reversedNum;
 }
 
+int decToBinary(int n )
+{
+	int returningInt=0;
+	while(n >= 1) {
+	int k = n % 2;
+	if(k == 1)
+	{
+		printf("1");
+		returningInt = (returningInt * 10) + 1;
+	}else
+	{
+		printf("0");
+		returningInt = returningInt * 10;
+	}
+	n = n / 2;
+	}
+	printf("\n");
+	printf("%d \n",returningInt);
+	return returningInt;
+}
+
+int binToDecimal(int n, int notationBase, int base)
+{
+	int num, k, dec_Val = 0,i = 1;
+	while(n > 0)
+	{
+		k = n % notationBase;
+		num = n / notationBase;
+		if(notationBase == 2)
+		{
+			if(k == 1)
+			{
+			dec_Val += 2^i;
+			printf("1");
+			
+			}
+			else if(k == 0)
+			{
+			printf("0");
+		
+			}
+		}
+	}
+	return dec_Val;
+
+}
 int main()
 {
 	int n = 32;
-	decToBinary(n);
+	int bin1 = decToBinary(n);
+	int dec1 = binToDecimal(bin1,10,2);
 	n = 1;
-	decToBinary(n);
+	int bin2 = decToBinary(n);
+	int dec2 = binToDecimal(bin2,10,2);
 	n = 1954;
-	decToBinary(n);
+	int bin3 = decToBinary(n);
+	int dec3 = binToDecimal(bin3,10,2);
 	n = 12345;
-	decToBinary(n);
+	int bin4 = decToBinary(n);
+	int dec4 = binToDecimal(bin4,10,2);
 }
